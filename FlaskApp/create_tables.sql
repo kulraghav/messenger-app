6eced76d78da830aef54b8c7b44579120ef9bd42
Paul Ventura <christianpaulventura@gmail.com>
2018-11-11 23:50:49 +0800

    add in auto increment and primary key for each tables

--- FlaskApp/create_tables.sql
+++ FlaskApp/create_tables.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (                                                                                                                                                                                        
-   user_id int(8),                                                                                                                                                                                          
-   username varchar(255),                                                                                                                                                                                   
-   password varchar(255)                                                                                                                                                                                    
-                                                                                                                                                                                                            
-);                                                                                                                                                                                                          
-                                                                                                                                                                                                            
-CREATE TABLE chats (                                                                                                                                                                                        
-   chat_id int(8),                                                                                                                                                                                          
-   text  varchar(255),                                                                                                                                                                                     
+   user_id int(8) NOT NULL AUTO_INCREMENT,                                                                                                                                                                                          
+   username varchar(255),
+   password varchar(255),
+   PRIMARY KEY (user_id)                                                                                                                                                                            
+);
 
-   from_uid int(8),                                                                                                                                                                                         
-   to_uid int(8),                                                                                                                                                                                           
-   created_at timestamp                                                                                                                                                                                     
-);      
+CREATE TABLE chats (                                                                                                                                                                                        
+   chat_id int(8) NOT NULL AUTO_INCREMENT,
+   text  varchar(255),
+   from_uid int(8),
+   to_uid int(8),
+   created_at timestamp,
+   PRIMARY KEY (chat_id)
+);
